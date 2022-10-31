27e9e5aa95f2a45008de7d6d373b3d9ac4f07fb5
akathor <erickmaster@gmail.com>
2022-10-31 15:26:23 +0100

    test

--- Prueba.java
+++ Prueba.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Prueba {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "E:/Mau QA/chromedriver.exe/");
+        System.setProperty("webdriver.chrome.driver", "./ui/resources/drivers/chromedriver.exe/");
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
